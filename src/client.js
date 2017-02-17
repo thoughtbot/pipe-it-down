bb716792145d7bd43addfe120cdc436297080708
Chris Thorn <chris@thorn.co>
2017-02-17 08:49:17 -0800

    Update icon when visibility is toggled
    Also extracts icons to separate file.

--- src/client.js
+++ src/client.js
@@ -1,7 +1,3 @@
-var icon = '<svg aria-hidden="true" class="octicon octicon-x" height="16" version="1.1" viewBox="0 0 12 16" width="12"> \
-  <path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"></path> \
-  </svg>';
-
 const tooltip = "Collapse this file";
 
 const link = $("<a />").attr({
@@ -10,7 +6,7 @@ const link = $("<a />").attr({
   "data-skip-pjax": "",
   "href": "#",
   "rel": "nofollow"
-}).html(icon);
+}).html(icons.hide);
 
 const github = new GitHub({ pathname: window.location.pathname });
 const store = new Store(github.pullRequestPath());
@@ -23,20 +19,23 @@ function decorateFile(file) {
   if ($(file).find(".pid-collapse").length === 0) {
     const fileName = $(file).find(".file-header").data("path");
     const fileContent = $(file).find(".js-file-content");
+    const fileLink = link.clone();
     store.getCollapsed(fileName).then(collapsed => {
       if (collapsed) {
+        fileLink.html(icons.show);
         fileContent.hide();
       }
     });
-    const fileLink = link.clone();
     fileLink.click(event => {
       event.preventDefault();
       if (fileContent.css("display") === "none") {
         fileContent.show();
         store.setUncollapsed(fileName);
+        fileLink.html(icons.hide);
       } else {
         fileContent.hide();
         store.setCollapsed(fileName);
+        fileLink.html(icons.show);
       }
     });
     $(file).find(".file-actions").append(fileLink);
---
+++ src/icons.js
@@ -0,0 +1,11 @@
+const icons = {
+  hide: '<svg aria-hidden="true" class="octicon octicon-x" height="16" version="1.1" viewBox="0 0 12 16" width="12"> \
+    <path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"> \
+    </path> \
+  </svg>',
+
+  show: '<svg aria-hidden="true" class="octicon octicon-eye" height="16" version="1.1" viewBox="0 0 16 16" width="16"> \
+    <path fill-rule="evenodd" d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"> \
+    </path> \
+  </svg>'
+};
