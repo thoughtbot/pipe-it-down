f22cfac0e2e9547941d2fc9c264913cb9d966016
Chris Thorn <chris@thorn.co>
2017-02-16 16:59:24 -0800

    Use ES6 features

--- client.js
+++ client.js
@@ -2,9 +2,9 @@ var icon = '<svg aria-hidden="true" class="octicon octicon-x" height="16" versio
   <path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"></path> \
   </svg>';
 
-var tooltip = "Collapse this file";
+const tooltip = "Collapse this file";
 
-var link = $("<a />").attr({
+const link = $("<a />").attr({
   "aria-label": tooltip,
   "class": "btn-octicon tooltipped tooltipped-nw pid-collapse",
   "data-skip-pjax": "",
@@ -13,16 +13,14 @@ var link = $("<a />").attr({
 }).html(icon);
 
 function decorateFiles() {
-  $(".file").each(function() {
-    decorateFile(this);
-  });
+  $(".file").forEach(file => decorateFile(file));
 }
 
 function decorateFile(file) {
   if ($(file).find(".pid-collapse").length === 0) {
-    var fileContent = $(file).find(".js-file-content");
-    var fileLink = link.clone();
-    fileLink.click(function(event) {
+    const fileContent = $(file).find(".js-file-content");
+    const fileLink = link.clone();
+    fileLink.click(event => {
       event.preventDefault();
       fileContent.toggle();
     });
@@ -30,10 +28,10 @@ function decorateFile(file) {
   }
 }
 
-var observer = new MutationObserver(() => decorateFiles());
+const observer = new MutationObserver(() => decorateFiles());
 
-$("[role=main]").each(function() {
-  observer.observe(this, { childList: true, subtree: true });
-});
+$("[role=main]").forEach(main =>
+  observer.observe(main, { childList: true, subtree: true })
+);
 
 decorateFiles();
